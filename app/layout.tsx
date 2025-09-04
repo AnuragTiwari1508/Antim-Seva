cd45ccbcd2e2a1eabb0163b26e939575b5a4628d
Vaibhav Malviya <vaibhavmalviya2902@gmail.com>
2025-09-04 19:06:31 +0530

    Adding Google Analytics in app->layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
-import './globals.css'
-import { Providers } from '@/app/providers'
+import "./globals.css"
+import { Providers } from "@/app/providers"
+import Script from "next/script"
 
 export default function RootLayout({
   children,
@@ -8,6 +9,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* ✅ Google Analytics */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-YG4KQ5XGJE"
+        />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-YG4KQ5XGJE');
+          `}
+        </Script>
+      </head>
       <body>
         <Providers>
           {children}
